6f2f0efa644cf65d5a0c9e20cb2566d0027d739e
Elan Ruusamäe <glen@delfi.ee>
2018-03-11 18:58:41 +0200

    route matcher: return only named keys
    needed for unit testing

--- src/Route/RouteMatcher.php
+++ src/Route/RouteMatcher.php
@@ -11,11 +11,20 @@ abstract class RouteMatcher
     {
         foreach ($this->getRoutes() as $route => $pattern) {
             if (preg_match("!{$pattern}!", $url, $matches)) {
-                return [$route, $matches];
+                return [$route, $this->getNamedKeys($matches)];
             }
         }
 
         throw new RouteNotMatchedException("Route not matched for {$url}");
+    }
+
+    private function getNamedKeys(array $array): array
+    {
+        $namedKeys = array_filter(array_keys($array), function ($element) {
+            return !is_numeric($element);
+        });
+
+        return array_intersect_key($array, array_flip($namedKeys));
     }
 
     abstract protected function getRoutes(): array;
