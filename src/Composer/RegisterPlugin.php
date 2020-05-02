3355158ddaf7abcd214273fa367a804391e07390
Elan Ruusamäe <glen@delfi.ee>
2020-05-02 09:12:25 +0300

    Add Composer plugin to register providers

---
+++ src/Composer/RegisterPlugin.php
@@ -0,0 +1,42 @@
+<?php
+declare(strict_types=1);
+
+namespace SlackUnfurl\Composer;
+
+use Composer\Composer;
+use Composer\EventDispatcher\EventSubscriberInterface;
+use Composer\IO\IOInterface;
+use Composer\Plugin\PluginInterface;
+use Composer\Script\Event;
+use Composer\Script\ScriptEvents;
+use RuntimeException;
+
+final class RegisterPlugin implements PluginInterface, EventSubscriberInterface
+{
+    public function activate(Composer $composer, IOInterface $io): void
+    {
+        // Nothing to do here, as all features are provided through event listeners
+    }
+
+    public function deactivate(Composer $composer, IOInterface $io): void
+    {
+        // Nothing to do here, as all features are provided through event listeners
+    }
+
+    public function uninstall(Composer $composer, IOInterface $io): void
+    {
+        // Nothing to do here, as all features are provided through event listeners
+    }
+
+    public static function getSubscribedEvents(): array
+    {
+        return [ScriptEvents::POST_AUTOLOAD_DUMP => 'registerPlugins'];
+    }
+
+    /**
+     * @throws RuntimeException
+     */
+    public static function registerPlugins(Event $composerEvent)
+    {
+    }
+}
