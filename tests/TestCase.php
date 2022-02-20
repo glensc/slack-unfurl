b32f9cd9c5f8ab95dda199646dd4040a7ba234dd
Elan Ruusamäe <glen@pld-linux.org>
2022-02-20 18:30:59 +0200

    Add createUnfurlEvent to TestCase

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -2,6 +2,26 @@
 
 namespace SlackUnfurl\Test;
 
+use SlackUnfurl\Event\UnfurlEvent;
+
 abstract class TestCase extends \PHPUnit\Framework\TestCase
 {
+    protected function createUnfurlEvent(string $url): UnfurlEvent
+    {
+        $domain = parse_url($url, PHP_URL_HOST);
+        $data = [
+            'type' => 'link_shared',
+            'user' => 'Uxxxxxxxx',
+            'channel' => 'Dxxxxxxxx',
+            'message_ts' => '1518125979.000251',
+            'links' => [
+                [
+                    'url' => $url,
+                    'domain' => $domain,
+                ],
+            ],
+        ];
+
+        return new UnfurlEvent($data);
+    }
 }
